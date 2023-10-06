8f4844d3137da44ee3bd3b6fca2ef42383778c0e
Vasu Vikram <vvikram@andrew.cmu.edu>
2023-10-06 17:20:45 -0400

    update pubs

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,10 +1,13 @@
 import React from "react"
+import { Link } from "gatsby"
 import Layout from "../components/layout"
 import "./projects.css"
 
 import ProjectItem from "../components/project-item"
 import bonsaiImg from "../../images/bonsai_graph.png"
 import mu2Img from "../../images/mu2.png"
+import unpinImg from "../../images/unpin.png"
+import proptestAiImg from "../../images/proptestai.png"
 import picassoImg from "../../images/picassoImg.png"
 
 export default function Research() {
@@ -13,11 +16,34 @@ export default function Research() {
       <h2>Projects</h2>
       <div id="project-container">
         <ul style={{ listStyle: `none`, float: `left`, display: `block` }}>
+          <ProjectItem img={unpinImg}
+                  alt="Pinning is Sinning" title="Pinning is Sinning: Towards Upgrading Maven Dependencies"
+                  link="/pdfs/unpin.pdf"
+                  authors="Vasudev Vikram, Yuvraj Agarwal, Rohan Padhye"
+                  event="Preprint">
+                <p className="project-summary">
+In this paper, we look to understand the frequency and consequences of dependency pinning. We conduct an empirical study to show that over 60% of consumers of popular Maven libraries pin their dependencies to outdated versions, some over a year old.
+Furthermore, these pinned versions often miss out on security fixes; we find that upgrading dependencies to the latest minor or patch version is 3.45x as likely to reduce security vulnerabilities rather than introduce new ones.
+We propose Unpin, a novel tool that computes a confidence score for a dependency upgrade by leveraging crowdsourced tests of peer projects and simulating the upgrade for them. Our evaluation on real-world pins to the top 500 popular libraries in Maven shows that Unpin (with a minimum confidence score of 5) can provide confidence to over 3,000 consumers to safely perform an upgrade that reduces security vulnerabilities.
+              </p>
+          </ProjectItem>
+          <ProjectItem img={proptestAiImg}
+                  alt="Proptest AI" title="Can Large Language Models Write Good Property-Based Tests?"
+                  link="https://arxiv.org/abs/2307.04346"
+                  authors="Vasudev Vikram, Caroline Lemieux, Rohan Padhye"
+                  event="Preprint">
+                <p className="project-summary">
+                   Property-based testing (PBT), while an established technique in the software testing research community, is still relatively underused in real-world software.
+                   Pain points in writing property-based tests include implementing diverse random input generators and thinking of meaningful properties to test. Developers, however, are more amenable to writing documentation; plenty of library API documentation is available and can be used as natural language specifications for property-based tests.
+                   As large language models (LLMs) have recently shown promise in a variety of coding tasks, we explore the potential of using LLMs to synthesize property-based tests. We call our approach Proptest AI, and propose three different strategies of prompting the LLM for PBT. We characterize various failure modes of
+                   Proptest AI and detail an evaluation methodology for automatically synthesized property-based tests. A prototype of our website can be found <Link to="https://proptest.ai">here</Link>.
+              </p>
+          </ProjectItem>
           <ProjectItem img={mu2Img}
                   alt="Mu2" title="Guiding Greybox Fuzzing with Mutation Testing"
                   link="/pdfs/mu2-issta23.pdf"
                   authors="Vasudev Vikram, Isabella Laybourn, Ao Li, Nicole Nair, Kelton OBrien, Rafaello Sanna, Rohan Padhye"
-                  event="ISSTA 2023">
+                  event="ISSTA 2023 (Distinguished Paper Award)">
                 <p className="project-summary">
                   In this paper, we develop and evaluate Mu2, a Java-based framework for incorporating mutation analysis in the greybox fuzzing loop, with the goal of producing a test-input corpus with a high mutation score.
                   Mu2 makes use of a differential oracle for identifying inputs that exercise interesting program behavior without causing crashes.
