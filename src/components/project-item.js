c29318943fa64a6497156d84defd7958152e83f7
Vasu Vikram <vasumvikram@gmail.com>
2024-11-04 11:12:55 -0500

    bold author

--- src/components/project-item.js
+++ src/components/project-item.js
@@ -3,21 +3,31 @@ import projectItemStyles from "./project-item.module.css"
 import { Link } from "gatsby"
 
 export default function ProjectItem(props) {
+  const formatAuthors = (authors) => {
+    return authors.split(', ').map((author, index) => {
+      if (author.trim() === "Vasudev Vikram") {
+        return <strong key={index}>{author}</strong>;
+      }
+      return author;
+    }).reduce((prev, curr) => [prev, ', ', curr]);
+  };
+
   var titleElem;
   if (props.titlenolink !== undefined) {
-    titleElem = <p className="titlenolink-text"> {props.titlenolink}> </p>
+    titleElem = <p className="titlenolink-text"> {props.titlenolink} </p>;
   } else {
-    titleElem = <Link to={props.link}> {props.title} </Link>
+    titleElem = <Link to={props.link}> {props.title} </Link>;
   }
+  
   return (
     <li className={projectItemStyles.projectitem}>
-        <img className="project" src={props.img} alt={props.alt}/>
-        <div className="project-item-text">
-            {titleElem}
-            <p className="authors-text">{props.authors}</p>
-            <p className="event-text">{props.event}</p>
-            {props.children}
-        </div>
+      <img className="project" src={props.img} alt={props.alt}/>
+      <div className="project-item-text">
+        {titleElem}
+        <p className="authors-text">{formatAuthors(props.authors)}</p>
+        <p className="event-text">{props.event}</p>
+        {props.children}
+      </div>
     </li>
-  )
+  );
 }
